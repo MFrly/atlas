437363b46fff9fb6aa406fcadaa40ed617df4092
Femmy :3 <118221254+MFrly@users.noreply.github.com>
2024-05-19 20:32:06 +0200

    Update atlas.js

--- atlas.js
+++ atlas.js
@@ -1,25 +1,65 @@
 const staty = document.getElementById('staty');
 
-fetch('https://restcountries.com/v3.1/region/europe')
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        //staty.innerHTML = data[9].translations.ces.common;
-        data.forEach(country => {
-            console.log(country.capital[0]);
-            let stat = `<div class="col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-6">
-            <div class="card">
-            <a href="${country.maps.googleMaps}">
-            <img class="card-img-top" src="${country.flags.svg}" alt="${country.flags.alt}" />
-            </a>
-                <div class="card-body">
-                    <h4 class="card-title">${country.translations.ces.common}</h4>
-                    <p class="card-text">${country.population}</p>
-                    <br> ${country.capital[0]}
-                </div>
-            </div>
-            
-        </div>`;
-            document.getElementById('staty').innerHTML+=stat;
+const selectSort = document.getElementById('selSort');
+
+function fetchDataAndRender(continent, sortBy) {
+    staty.innerHTML = '';
+
+    fetch(`https://restcountries.com/v3.1/region/${continent}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            data.sort((a, b) => {
+                if (sortBy === 'name') {
+                    const nameA = a.translations.ces ? a.translations.ces.common : a.name.official;
+                    const nameB = b.translations.ces ? b.translations.ces.common : b.name.official;
+                    return nameA.localeCompare(nameB);
+                } else if (sortBy === 'population') {
+                    return b.population - a.population;
+                } else if (sortBy === 'area') {
+                    return b.area - a.area;
+                }
+            });
+
+            data.forEach(stat => {
+                let blockCountry = `
+                <div class="col-xl-2 col-lg-2 col-md-4 col-sm-6">
+                    <div class="card country-card">
+                        <a href="${stat.maps.googleMaps}" target="_blank">
+                            <img class="card-img-top" src="${stat.flags.png}" alt="${stat.name.official}" />
+                        </a>
+                        <div class="card-body">
+                            <h4 class="card-title">${stat.translations.ces ? stat.translations.ces.common : stat.name.official}</h4>
+                            <p class="card-text">Počet obyvatel: ${stat.population}
+                                <br>Rozloha: ${stat.area} km<sup>2</sup>
+                            </p>
+                        </div>
+                    </div>                   
+                </div>`;
+                staty.innerHTML += blockCountry;
+            });
+        })
+        .catch((error) => {
+            console.error('There has been a problem with your fetch operation:', error);
+            staty.innerHTML = `<p class="text-danger">Nastala chyba při načítání dat. Zkuste to prosím později.</p>`;
         });
-    });
+}
+
+const selectContinent = document.getElementById('selContinent');
+selectContinent.addEventListener('change', function() {
+    const selectedContinent = this.value;
+    const sortBy = selectSort.value;
+    fetchDataAndRender(selectedContinent, sortBy);
+});
+
+selectSort.addEventListener('change', function() {
+    const selectedContinent = selectContinent.value;
+    const sortBy = this.value;
+    fetchDataAndRender(selectedContinent, sortBy);
+});
+
+fetchDataAndRender(selectContinent.value, selectSort.value);
